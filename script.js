4b35975aba62e4784e2575cb540a7d96de246554
Rahat <rahathassan934@gmail.com>
2023-12-14 23:52:49 +0600

    commit

--- script.js
+++ script.js
@@ -31,23 +31,47 @@ function checkUncheck() {
 
 
 // =================== Light Dark toggle ==============
-// var darkMode = document.getElementById('dark');
-// darkMode.addEventListener('click', function(){
-//     darkMode.classList.toggle('active');
-// })
-// // is DarkMode on then dark color pelete position
-// var darkOn = document.querySelector('.darkMode');
-// var darkpalette = document.querySelector('.themeDark');
-// darkOn.onclick = function(){
-//     darkpalette.classList.toggle('activee')
-// }
+var darkMode = document.getElementById('dark');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+})
+// is DarkMode on then dark color pelete position
+var darkOn = document.querySelector('.darkMode');
+var darkpalette = document.querySelector('.themeDark');
+darkOn.onclick = function(){
+    darkpalette.classList.toggle('activee')
+}
 
 
 // ============  theme colro switche by https://fossheim.io/writing/posts/accessible-theme-picker-html-css-js/
 
 // selects all buttons
-const themeColor = document.querySelector('.themeColorButton');
-const themeColorButton = themeColor.querySelectorAll('button');
+const themeColorLight = document.querySelector('.themeLight');
+const themeColorButtonLight = themeColorLight.querySelectorAll('button');
+
+// Logs the clicked button
+const handleThemeSelectionLight = (event) =>{
+    const theme = event.target.getAttribute('data-theme')
+    const isPressrd = event.target.getAttribute('aria-pressed');
+    document.documentElement.setAttribute("data-selected-theme",theme);
+
+    // delete 'aria-pressed="true" ' form previous button
+    const prevBtn = document.querySelector('[data-theme][aria-pressed="true"]');
+    prevBtn.setAttribute('aria-pressed', false);
+    event.target.setAttribute('aria-pressed', true);
+
+}
+// Adds the handleThemeSelection as a click handler to each of the buttons
+
+themeColorButtonLight.forEach((button) =>{
+    button.addEventListener('click', handleThemeSelectionLight);
+    // document.documentElement.setAttribute("data-theme", button)
+});
+
+
+// selects all buttons
+const themeColorDark = document.querySelector('.themeDark');
+const themeColorButtonDark = themeColorDark.querySelectorAll('button');
 
 // Logs the clicked button
 const handleThemeSelection = (event) =>{
@@ -63,7 +87,7 @@ const handleThemeSelection = (event) =>{
 }
 // Adds the handleThemeSelection as a click handler to each of the buttons
 
-themeColorButton.forEach((button) =>{
-    button.addEventListener('click', handleThemeSelection);
+themeColorButtonDark.forEach((button) =>{
+    button.addEventListener('click', handleThemeSelectionDark);
     // document.documentElement.setAttribute("data-theme", button)
 });
